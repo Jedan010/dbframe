f55d24904e719e4af6869edbf00c8852bce2c975
jiedewu <wujiede@csc.com.cn>
2022-09-16 12:46:27 +0800

    add: 可以不使用mongo和clickhouse数据库

--- dbframe/__init__.py
+++ dbframe/__init__.py
@@ -1,5 +1,11 @@
-from dbframe.clickhouse_api import ClickHouseDB, read_ch, save_ch
+try:
+    from dbframe.clickhouse_api import ClickHouseDB, read_ch, save_ch
+except:
+    pass
 from dbframe.database_api import read_df, save_df
 from dbframe.hdfs_api import HdfsDB, read_h5, save_h5
-from dbframe.mongo_api import MongoDataFrameDB, read_mongo, save_mongo
+try:
+    from dbframe.mongo_api import MongoDataFrameDB, read_mongo, save_mongo
+except:
+    pass
 from dbframe.mysql_api import MysqlDB, read_sql, save_sql
