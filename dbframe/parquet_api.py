7d440b0d598e027af0c11cd3bf8e395bb7361f84
wujiede <547054738@qq.com>
2024-10-24 23:52:55 +0800

    chg: 改写parquet数据库操作

--- dbframe/parquet_api.py
+++ dbframe/parquet_api.py
@@ -1,11 +1,13 @@
 import os
 from copy import deepcopy
-from functools import lru_cache
+from dbframe.cache import lru_cache
 from typing import Callable
 
 import pandas as pd
 from pyarrow.lib import concat_tables
-from pyarrow.parquet import read_table as read_parquet
+from pyarrow.parquet import read_table, read_schema
+from pyarrow.lib import TimestampType
+from fastparquet import write
 
 from dbframe.database_api import DatabaseTemplate
 from dbframe.setting import CACHE_SIZE
@@ -24,76 +26,95 @@ class ParquetDB(DatabaseTemplate):
         cache_size: int = CACHE_SIZE,
     ):
         self.base_dir = path_data
-        self.file_names = os.listdir(self.base_dir)
-        self.date_names = [name.split(".")[0] for name in self.file_names]
-        self._file_df = pd.Series(
-            data=self.file_names, index=pd.to_datetime(self.date_names)
-        ).sort_index()
 
         self._read_df_cache = lru_cache(maxsize=cache_size)(self._read_df)
 
     @property
     def tables(self):
-        return self.date_names
+        return [
+            name[:-8] for name in os.listdir(self.base_dir) if name.endswith(".parquet")
+        ]
 
     def _read_df(
         self,
-        start: str,
-        end: str,
+        table: str,
+        start: str = None,
+        end: str = None,
         fields: list[str] = None,
         symbols: list[str] = None,
-        query: list[tuple[str,str, str]] = None,
-        date_filter:Callable = None,
-        index_col: list[str] = None,
+        query: list[tuple[str, str, str]] = None,
+        date_name: str = None,
+        index_col: list[str] = "auto",
         is_sort_index: bool = True,
         is_drop_duplicate_index: bool = False,
         **kwargs,
-    ):
+    ) -> pd.DataFrame:
         """
         读取 parquet 数据
         """
-        files:pd.Series = self._file_df.loc[start:end]
-        if files.empty:
+
+        if table not in self.tables:
             return pd.DataFrame()
+
+        path_table = path = os.path.join(self.base_dir, table + ".parquet")
+        metadata = read_schema(path_table)
+        cols: list[str] = metadata.names
+
+        if index_col == "auto":
+            index_col: list[str] = metadata.pandas_metadata["index_columns"]
+        elif index_col is not None:
+            if index_col is None:
+                if isinstance(index_col, (int, str)):
+                    index_col = [index_col]
+            index_col = [cols[c] if isinstance(c, int) else c for c in index_col]
+
+        if index_col is not None and index_col != "auto":
+            index_col_strict = [c for c in index_col if c in cols]
+
+        if date_name is None:
+            date_name = "date"
+            if "date" not in cols and "datetime" in cols:
+                date_name = "datetime"
+
+        if "columns" in kwargs:
+            _columns = kwargs.get("columns")
+            del kwargs["columns"]
+            if fields is None:
+                fields = _columns
+
+        if fields is not None:
+            if isinstance(fields, str):
+                fields = [fields]
+            fields: pd.Index = pd.Index(fields)
+            fields = fields.union(index_col_strict, sort=False).tolist()
 
         if query is None:
             query = []
         if isinstance(query, str):
             query = [query]
 
+        if isinstance(
+            metadata.types[metadata.get_field_index(date_name)], TimestampType
+        ):
+            if start is not None:
+                query.append((date_name, ">=", pd.to_datetime(start)))
+            if end is not None:
+                query.append((date_name, "<=", pd.to_datetime(end)))
+
         if symbols is not None:
             if isinstance(symbols, str):
                 symbols = [symbols]
             query.append(("symbol", "in", symbols))
 
-        if fields is not None:
-            if isinstance(fields, str):
-                fields = [fields]
-            if index_col is not None:
-                if isinstance(index_col, str):
-                    index_col = [index_col]
-                fields = pd.Index(fields).union(index_col, sort=False).tolist()
+        if len(query) == 0:
+            query = None
 
-        tables = []
-        for date, file_name in files.items():
-            path = os.path.join(self.base_dir, file_name)
-            filters = deepcopy(query)
-            if date_filter is not None and callable(date_filter):
-                _filter = date_filter(date)
-                if isinstance(_filter, tuple):
-                    filters.append(_filter)
-                elif isinstance(_filter, list):
-                    filters.extend(_filter)
-            if len(filters) == 0:
-                filters = None
-            table = read_parquet(path, columns=fields, filters=filters, **kwargs)
-            tables.append(table)    
+        parquet_table = read_table(path, columns=fields, filters=query, **kwargs)
+        df: pd.DataFrame = parquet_table.to_pandas()
 
-        py_table = concat_tables(tables)
-        df: pd.DataFrame = py_table.to_pandas()
-
-        if index_col is not None:
-            df = df.set_index(index_col)
+        if index_col:
+            if df.index.names != index_col:
+                df = df.reset_index().set_index(index_col)
             if is_sort_index:
                 df = df.sort_index()
             if is_drop_duplicate_index:
@@ -103,12 +124,14 @@ class ParquetDB(DatabaseTemplate):
 
     def read_df(
         self,
-        start: str,
-        end: str,
+        table: str,
+        start: str = None,
+        end: str = None,
         fields: list[str] = None,
         symbols: list[str] = None,
         query: list[tuple[str, str, str]] = None,
-        index_col: list[str] = None,
+        date_name: str = None,
+        index_col: list[str] = "auto",
         is_sort_index: bool = True,
         is_drop_duplicate_index: bool = False,
         is_cache: bool = False,
@@ -152,11 +175,13 @@ class ParquetDB(DatabaseTemplate):
         else:
             func = self._read_df
         return func(
+            table=table,
             start=start,
             end=end,
             fields=fields,
             symbols=symbols,
             query=query,
+            date_name=date_name,
             index_col=index_col,
             is_sort_index=is_sort_index,
             is_drop_duplicate_index=is_drop_duplicate_index,
@@ -164,4 +189,18 @@ class ParquetDB(DatabaseTemplate):
         )
 
     def save_df(self, df: pd.DataFrame, table: str, **kwargs) -> bool:
-        pass
+        """
+        保存 datafraome 数据至 parquet 文件
+        """
+
+        if df.empty:
+            return
+
+        if isinstance(df, pd.Series):
+            df = df.to_frame()
+
+        path_table = os.path.join(self.base_dir, table + ".parquet")
+        if table not in self.tables:
+            return df.to_parquet(path_table, **kwargs)
+        else:
+            return write(filename=path_table, data=df, append=True)
